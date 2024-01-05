3531ad8bdd876a4b24e2767b8d81a786cdcc5d43
FatCat-BOY <fm_boy@qq.com>
2024-01-05 20:55:22 +0800

    2024-01-05 更新页面+部署

--- js/index.js
+++ js/index.js
@@ -4,15 +4,10 @@ let App = new Vue({
     data: {
         // 提供数据
         imgList: [
-            { img: `url('./resources/image/Cat/001.jpg` },
-            { img: `url('./resources/image/Cat/002.jpg` },
-            { img: `url('./resources/image/Cat/003.jpg` },
-            { img: `url('./resources/image/Cat/004.jpg` },
-            { img: `url('./resources/image/Cat/005.jpg` },
-            { img: `url('./resources/image/Cat/006.jpg` },
-            { img: `url('./resources/image/Cat/007.jpg` },
-            { img: `url('./resources/image/Cat/008.jpg` },
-            { img: `url('./resources/image/Cat/009.jpg` },
+            { img: './resources/image/Cat/001.jpg' },
+            { img: './resources/image/Cat/002.jpg' },
+            { img: './resources/image/Cat/003.jpg' },
+            { img: './resources/image/Cat/004.jpg' },
         ],
         carouselList: [
             { url: 'https://fanyi.baidu.com/', text: '百度翻译' },
@@ -21,7 +16,6 @@ let App = new Vue({
             { url: 'https://gitee.com/', text: 'Gitee' },
             { url: 'https://github.com/login', text: 'GitHub' },
             { url: 'https://chat18.aichatos.xyz/', text: 'AIcharOS' },
-            { url: 'https://www.csdn.net/', text: 'CSDN' },
         ],
     },
     methods: {
@@ -29,27 +23,27 @@ let App = new Vue({
     }
 })
 
-let ul = document.querySelector('.body-carousel-img ul')
+// 轮播图
+let div = document.querySelector('.body-carousel-img div')
 // 克隆第一张图片
-let clonefirstImg = ul.firstElementChild.cloneNode()
+let clonefirstImg = div.firstElementChild.cloneNode()
 // 将第一张图片添加至图片列表的末尾
-ul.appendChild(clonefirstImg)
-// 图片索引 代表当前是第几张图片  index:0代表第一张图片
+div.appendChild(clonefirstImg)
+
+// 图片索引
 let index = 0
 // 自动轮播
-let lock = true;
 setInterval(() => {
-    if (!lock) return;
     index++;
-    ul.style.left = index * -100 + "%";
-    ul.style.transition = "0.5s ease";
-    if (index === 9) {
+    div.style.transition = ".8s";
+    div.style.left = index * -100 + "%";
+    if (index === App.imgList.length) {
         index = 0;
         setTimeout(() => {
-            ul.style.left = 0;
-            // 取消过渡 500毫秒之后切换第一张
-            ul.style.transition = "none";
-        }, 500);
+            div.style.left = 0;
+            // 取消过渡 800毫秒之后切换第一张
+            div.style.transition = "none";
+        }, 800);
     }
 }, 2000);
 /* -------------------- **** -------------------- */
