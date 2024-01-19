65c3355ac617e77f808a6a4a61200613fa58f8d8
FatCat-BOY <fm_boy@qq.com>
2024-01-20 00:08:17 +0800

    2024-01-20 整理文件夹，更新CommonWeb页面

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 /* -------------------- **** -------------------- */
-let App = new Vue({
-    el: '#App',
+const app = new Vue({
+    el: '#app',
     data: {
-        // 提供数据
+        // 轮播图图片
         imgList: [
-            { img: './resources/image/Cat/001.jpg' },
-            { img: './resources/image/Cat/002.jpg' },
-            { img: './resources/image/Cat/003.jpg' },
-            { img: './resources/image/Cat/004.jpg' },
+            { img: './public/resources/image/Cat/001.jpg' },
+            { img: './public/resources/image/Cat/002.jpg' },
+            { img: './public/resources/image/Cat/003.jpg' },
+            { img: './public/resources/image/Cat/004.jpg' },
         ],
-        carouselList: [
+        commonList: [
             { url: 'https://fanyi.baidu.com/', text: '百度翻译' },
             { url: 'https://www.bilibili.com/', text: '哔哩哔哩' },
             { url: 'https://www.csdn.net/', text: 'CSDN' },
@@ -17,10 +17,13 @@ let App = new Vue({
             { url: 'https://github.com/login', text: 'GitHub' },
             { url: 'https://chat18.aichatos.xyz/', text: 'AIcharOS' },
         ],
+        toolsList: [
+            { url: 'https://pc.woozooo.com/', text: '蓝奏·云存储' },
+            { url: 'https://www.speedtest.cn/', text: '测速网' },
+            { url: 'https://cli.im/', text: '草料二维码生成器' },
+            { url: 'http://zhouql.vip/bilibili/', text: 'B站视频下载工具 | 极简纯净' },
+        ]
     },
-    methods: {
-        // 提供处理逻辑函数
-    }
 })
 
 // 轮播图
@@ -29,15 +32,14 @@ let div = document.querySelector('.body-carousel-img div')
 let clonefirstImg = div.firstElementChild.cloneNode()
 // 将第一张图片添加至图片列表的末尾
 div.appendChild(clonefirstImg)
-
 // 图片索引
 let index = 0
 // 自动轮播
 setInterval(() => {
-    index++;
-    div.style.transition = ".8s";
+    index++
+    div.style.transition = ".8s"
     div.style.left = index * -100 + "%";
-    if (index === App.imgList.length) {
+    if (index === app.imgList.length) {
         index = 0;
         setTimeout(() => {
             div.style.left = 0;
