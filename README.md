ba65eea80fa0545d42fcf74406be4123ec9d7e39
FatCat-BOY <fm_boy@qq.com>
2024-02-14 13:59:01 +0800

    2024-02-14 整理文件夹，更新首页，添加搜索功能

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # FatCat-BOY 个人主栈
 
-#### 介绍
+<img src="./public/resources/image/FM.jpg" alt="FMBOY" style="zoom: 20%;" />
+
+#### —— 介绍 ——
+
 仓库于 2024年01月01 日创建
 
-一个人的行动，决定一个人的未来 ~ 期待我的完善
+此仓库用于 分享：**网站、源码、工具等**
 
-#### 作者
+
+
+团队仓库地址：[**点击跳转**](https://gitee.com/CatFM/Team)
+
+CSDN地址：[**点击跳转**](https://blog.csdn.net/m0_74175846)
+
+个人QQ邮箱：**FM_BOY@qq.com**
+
+------
+
+#### —— 作者 ——
 FatCat-BOY
--- js/index.js
+++ js/index.js
@@ -1,51 +1,57 @@
-/* -------------------- **** -------------------- */
 const app = new Vue({
     el: '#app',
     data: {
-        // 轮播图图片
-        imgList: [
-            { img: './public/resources/image/Cat/001.jpg' },
-            { img: './public/resources/image/Cat/002.jpg' },
-            { img: './public/resources/image/Cat/003.jpg' },
-            { img: './public/resources/image/Cat/004.jpg' },
+        // 搜索框数据
+        url: 'https://www.baidu.com/s',
+        text: '百度一下',
+        name: 'wd',
+        inpValue: '',
+        searchList: [
+            { title: '百度', url: 'https://www.baidu.com/s', text: '百度一下', name: 'wd' },
+            { title: 'Bing', url: 'https://cn.bing.com/search', text: '微软Bing搜索', name: 'q' }
         ],
-        commonList: [
-            { url: 'https://fanyi.baidu.com/', text: '百度翻译' },
-            { url: 'https://www.bilibili.com/', text: '哔哩哔哩' },
-            { url: 'https://www.csdn.net/', text: 'CSDN' },
-            { url: 'https://gitee.com/', text: 'Gitee' },
-            { url: 'https://github.com/login', text: 'GitHub' },
-            { url: 'https://chat18.aichatos.xyz/', text: 'AIcharOS' },
-        ],
-        toolsList: [
-            { url: 'https://pc.woozooo.com/', text: '蓝奏·云存储' },
-            { url: 'https://www.speedtest.cn/', text: '测速网' },
-            { url: 'https://cli.im/', text: '草料二维码生成器' },
-            { url: 'http://zhouql.vip/bilibili/', text: 'B站视频下载工具 | 极简纯净' },
+        tagList: [
+            [
+                { url: 'https://fanyi.baidu.com/', img: 'https://fanyi-cdn.cdn.bcebos.com/webStatic/translation/img/favicon/favicon.ico', text: '百度翻译' },
+                { url: 'https://www.bilibili.com/', img: 'https://www.bilibili.com/favicon.ico', text: '哔哩哔哩' },
+                { url: 'https://www.csdn.net/', img: 'https://g.csdnimg.cn/static/logo/favicon32.ico', text: 'CSDN' },
+            ],
         ]
     },
-})
-
-// 轮播图
-let div = document.querySelector('.body-carousel-img div')
-// 克隆第一张图片
-let clonefirstImg = div.firstElementChild.cloneNode()
-// 将第一张图片添加至图片列表的末尾
-div.appendChild(clonefirstImg)
-// 图片索引
-let index = 0
-// 自动轮播
-setInterval(() => {
-    index++
-    div.style.transition = ".8s"
-    div.style.left = index * -100 + "%";
-    if (index === app.imgList.length) {
-        index = 0;
-        setTimeout(() => {
-            div.style.left = 0;
-            // 取消过渡 800毫秒之后切换第一张
-            div.style.transition = "none";
-        }, 800);
+    methods: {
+        // 搜索列表切换方法
+        searchListBtn(e) {
+            if (e.target.tagName === 'A') {
+                const data = this.searchList.filter(item => item.title === e.target.innerText)
+                const { url, text, name } = data[0]
+                this.url = url
+                this.text = text
+                this.name = name
+                document.querySelector('.list-btn-on').classList.remove('list-btn-on')
+                e.target.classList.add('list-btn-on')
+                this.inpOn()
+            }
+        },
+        // 判断搜索方法
+        searchBtn(e) {
+            const value = this.inpValue.trim()
+            if (value) {
+                this.inpValue = value
+            } else {
+                this.inpValue = ''
+                e.preventDefault()
+            }
+        },
+        // 自动选中输入框方法
+        inpOn() {
+            const inp = document.querySelector('.main-search form input:nth-child(1)')
+            inp.focus()
+        }
+    },
+    created() {
+    },
+    mounted() {
+        // 进页面自动选中输入框
+        this.inpOn()
     }
-}, 2000);
-/* -------------------- **** -------------------- */
+})
